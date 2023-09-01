3d63a702579f0bb285e3c73c49fa1130836a19cb
isaacy02 <142541078+isaacy02@users.noreply.github.com>
2023-08-31 20:51:45 -0400

    Add files via upload

---
+++ ch02_isaac_young.js
@@ -0,0 +1,36 @@
+// Isaac Young
+// Chapter 2
+
+// Looping a triangle
+
+let line = "#";
+while(line.length <= 7) {
+    console.log(line);
+    line = line + "#"
+}
+
+// Fizz Buzz
+for (let n = 1; n <= 100; n++) {
+    let output = "";
+    if (n % 3 == 0) output += "Fizz";
+    if (n % 5 == 0) output += "Buzz";
+    console.log(output || n);
+  }
+  
+
+
+// Chessboard
+
+let size = 8;
+let board = "";
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+console.log(board);
